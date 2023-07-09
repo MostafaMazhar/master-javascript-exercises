42b9ea04c93dc90d9db85d974a5b8e615e18c3c5
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-07-09 16:20:04 -0400

    are both odd exercise

--- exercises/021-areBothOdd/app.js
+++ exercises/021-areBothOdd/app.js
@@ -1,1 +1,9 @@
 // Write your function here
+function areBothOdd(a,b){
+     if(a % 2 !== 0 && b % 2 !== 0){
+      return true;
+     }
+     return false;
+}
+let output = areBothOdd(1, 3);
+console.log(output); // --> true

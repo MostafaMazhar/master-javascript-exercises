18b5acca42a71990d816e9f334c7359c8b1be211
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-07-09 17:17:37 -0400

    25 isEvenAndGreaterThanTen

--- exercises/025-isEvenAndGreaterThanTen/app.js
+++ exercises/025-isEvenAndGreaterThanTen/app.js
@@ -1,1 +1,12 @@
 // Write your function here
+function isEvenAndGreaterThanTen(n){
+  if(n % 2 === 0 && n > 10){
+     return true
+  }
+  return false;
+}
+
+
+
+let output = isEvenAndGreaterThanTen(13);
+console.log(output); // --> false
